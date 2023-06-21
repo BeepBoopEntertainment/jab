2bac7f1d2c0255f68609da35e96559920dffc5c3
joshuaGlass808 <joshua.lee.glass@gmail.com>
2023-06-20 23:04:17 -0700

    some more things for map generation

--- src/tilemaps/tile_map.gd
+++ src/tilemaps/tile_map.gd
@@ -5,6 +5,16 @@ var temperature = FastNoiseLite.new()
 var altitude = FastNoiseLite.new()
 var width = 256
 var height = 256
+
+const LayerIds = {
+	BASE = 0,
+	OBJECT = 1
+}
+
+const SourceIds = {
+	FARMLANDS = 0,
+	WETLANDS  = 1
+}
 #@onready var player = get_parent().get_child(1)
 
 func _ready():
@@ -16,23 +26,68 @@ func _ready():
 
 func _process(delta):
 	generate_chunk(Vector2(1,1))
+
+
+func map_environment_to_tile(alt: float, moist: float, temp: float):
+	var source_id: int = SourceIds.FARMLANDS
+	var at_coord: Vector2 = Vector2(1,1)
+	if alt < -3 and moist > 4 and temp > -3:
+		at_coord = Vector2(30,1)
+		source_id = SourceIds.WETLANDS
+	elif alt <= -3 and moist > 4 and temp <= -3:
+		at_coord = Vector2(27,4)
+		source_id = SourceIds.WETLANDS
+	elif moist > 4 and temp > -3 and alt > -3:
+		at_coord = Vector2(4,1)
+		source_id = SourceIds.WETLANDS
+	elif alt > -3 and moist <= 4 and temp > -3 and temp <= -1:
+		at_coord = Vector2(2,0)
+		source_id = SourceIds.FARMLANDS
+	elif alt > -3 and moist <= 4 and temp > -1 and temp <= 0:
+		at_coord = Vector2(3,0)
+		source_id = SourceIds.FARMLANDS
+	elif moist > 1 and temp > 2:
+		at_coord = Vector2(0,0)
+		source_id = SourceIds.FARMLANDS
+	elif moist <= 1 and temp > 2:
+		at_coord = Vector2(1,0)
+		source_id = SourceIds.FARMLANDS
+	else:
+		at_coord = Vector2(4,1)
+		source_id = SourceIds.WETLANDS
 	
+	return {
+		coord  = at_coord,
+		source = source_id
+	}
+
+
+func get_environment(tile_pos, x, y):
+	var xnoise: float = tile_pos.x-width/2 + x
+	var ynoise: float = tile_pos.y-height/2 + y
+	var moist: float = moisture.get_noise_2d(xnoise, ynoise)*10
+	var temp: float = temperature.get_noise_2d(xnoise, ynoise)*10
+	var alt: float = altitude.get_noise_2d(xnoise, ynoise)*10
+	
+	return {
+		alt   = alt,
+		coord = Vector2i(xnoise, ynoise),
+		moist = moist,
+		temp  = temp
+	}
+
 
 func generate_chunk(position):
 	var tile_pos = local_to_map(position)
 	for x in range(width):
 		for y in range(height):
-			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-			var alt = abs(altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10)
-			var coord = Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)
-			# create enum mapping for Vector2 calls below.
-			
-			
-			if alt < 2.5 and moist > 1:
-				set_cell(0, coord, 0, Vector2(2,0))
-			elif moist < 1:
-				set_cell(0, coord, 1, Vector2(2,2))
-			else:
-				set_cell(0, coord, 1, Vector2(20,2))
+			var environment = get_environment(tile_pos, x, y)
+			var layer_id: int = LayerIds.BASE
+			var tile = map_environment_to_tile(
+				environment.alt,
+				environment.moist,
+				environment.temp
+			)
+				
+			set_cell(layer_id, environment.coord, tile.source, tile.coord)
 
