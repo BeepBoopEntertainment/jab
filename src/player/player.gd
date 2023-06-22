6259c95727877b4e9d663564aa88d810c2549e4a
joshuaGlass808 <joshua.lee.glass@gmail.com>
2023-06-21 19:22:09 -0700

    simple player animation (#8)
    * simple player animation
    
    * updating code

--- src/player/player.gd
+++ src/player/player.gd
@@ -6,4 +6,21 @@ func _physics_process(_delta):
 	velocity.x = Input.get_axis("ui_left", "ui_right")
 	velocity.y = Input.get_axis("ui_up", "ui_down")
 	velocity = velocity.normalized() * SPEED
+	
+	var selected = null
+	if Input.is_key_pressed(KEY_UP):
+		selected = "forward"
+	if Input.is_key_pressed(KEY_DOWN):
+		selected = "backward"
+	if Input.is_key_pressed(KEY_LEFT):
+		selected = "left"
+	if Input.is_key_pressed(KEY_RIGHT):
+		selected = "right"
+		
+	if selected == null:
+		$AnimatedSprite2D.speed_scale = 0
+	else:
+		$AnimatedSprite2D.play(selected)
+		$AnimatedSprite2D.speed_scale = 1
+	
 	move_and_slide()
